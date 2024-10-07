47922ee9fa0e8f3f56c3ca3222f0bcc25af58c4f
nthndy <day.nathan@pm.me>
2024-10-07 17:41:23 +0100

    inspecting rif cases and negative dts

--- macrohet/colours.py
+++ macrohet/colours.py
@@ -30,8 +30,32 @@ expanded_piyg = [
     '#d02c91'   # Last color from sns.color_palette('PiYG')
 ]
 
+vaporwave = [
+    '#D02C91',  # Purple Sunset Glow: A rich, warm purple, like the vibrant colors of a sunset.
+    '#F1C2F2',  # Blush of the Morning Sky: A soft pastel pink, like the first light of dawn.
+    '#C291F2',  # Lavender Breeze: A soothing lavender, like the gentle breeze over a lavender field.
+    '#564D8C',  # Midnight Mountain: A deep, cool purple, evoking the silhouette of mountains under the night sky.
+    '#57AAF2',  # Ocean's Whisper: A tranquil blue, reflecting the calm of the sea.
+    '#A0D9D9',  # Misty Lagoon: A soft, serene blue-green, like mist hovering over a quiet lagoon.
+    '#A6D96A',  # Spring Meadow: A vibrant green, capturing the freshness of a meadow in spring.
+    '#1A9641'   # Pine Grove Green: A deep, earthy green, reminiscent of a dense pine forest.
+]
+
+mint_taupe = [
+    '#0D1321',  # Midnight Horizon: A deep, dark navy, like the sky just before dawn.
+    '#1D8FE0',  # Cerulean Splash: A bright, refreshing blue, reminiscent of a clear, sunny day at the ocean.
+    '#C5D86D',  # Meadow Mist: A soft, gentle green, like dew on the early morning grass.
+    '#ADD9C5',  # Seafoam Whisper: A calming, muted aqua, like the soft lull of ocean waves.
+    '#FFEDDF',  # Morning Glow: A light, warm peach, like the first rays of sunlight touching the clouds.
+    '#8C6764',  # Rustic Canyon: An earthy, rich brown, reminiscent of the hues of a rugged canyon landscape.
+    '#CC5105',  # Autumn Ember: A fiery orange, like the vibrant leaves of fall or the warmth of a glowing hearth.
+    '#8D7494'   # Dusky Plum: A muted purple, like the soft twilight just before the night fully sets in.
+]
+
 # Define the custom_colours dictionary
 custom_colours = {
     'lavender_raisin': lavender_raisin,
-    'expanded_piyg': expanded_piyg
+    'expanded_piyg': expanded_piyg,
+    'vaporwave': vaporwave,
+    'mint_taupe': mint_taupe
 }
