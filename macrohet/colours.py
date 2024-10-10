3d67cff75de0f101ac43e87860d67238b292cd3c
nthndy <day.nathan@pm.me>
2024-10-10 17:55:46 +0100

    statistical investigations

--- macrohet/colours.py
+++ macrohet/colours.py
@@ -6,9 +6,11 @@ This module defines custom color maps for visualizations.
 Custom Color Maps:
 - lavender_raisin: A color map featuring shades of lavender raisin.
 - expanded_piyg: An expanded version of the 'PiYG' color map.
+- vaporwave: A vaporwave-inspired color map.
+- mint_taupe: A color map inspired by natural tones of mint and taupe.
+- yellow_pig: A new color map featuring a combination of vibrant colors.
 
 Each color map is represented as a list of color codes.
-
 """
 
 # Define the lavender_raisin color map
@@ -30,6 +32,7 @@ expanded_piyg = [
     '#d02c91'   # Last color from sns.color_palette('PiYG')
 ]
 
+# Define the vaporwave color map
 vaporwave = [
     '#D02C91',  # Purple Sunset Glow: A rich, warm purple, like the vibrant colors of a sunset.
     '#F1C2F2',  # Blush of the Morning Sky: A soft pastel pink, like the first light of dawn.
@@ -41,6 +44,7 @@ vaporwave = [
     '#1A9641'   # Pine Grove Green: A deep, earthy green, reminiscent of a dense pine forest.
 ]
 
+# Define the mint_taupe color map
 mint_taupe = [
     '#0D1321',  # Midnight Horizon: A deep, dark navy, like the sky just before dawn.
     '#1D8FE0',  # Cerulean Splash: A bright, refreshing blue, reminiscent of a clear, sunny day at the ocean.
@@ -52,10 +56,23 @@ mint_taupe = [
     '#8D7494'   # Dusky Plum: A muted purple, like the soft twilight just before the night fully sets in.
 ]
 
+# Define the super_expiyg color map (extracted from the provided image)
+super_expiyg = [
+    '#1A9641',  # Deep Green
+    '#7BDAA4',  # Mint Green
+    '#F0BE38',  # Golden Yellow
+    '#F0D795',  # Light Beige
+    '#EF93B9',  # Soft Pink
+    '#D12B82',  # Bright Magenta
+    '#9F9BC7',  # Lavender Blue
+    '#452A61'   # Dark Purple
+]
+
 # Define the custom_colours dictionary
 custom_colours = {
     'lavender_raisin': lavender_raisin,
     'expanded_piyg': expanded_piyg,
     'vaporwave': vaporwave,
-    'mint_taupe': mint_taupe
+    'mint_taupe': mint_taupe,
+    'super_expiyg': super_expiyg
 }
